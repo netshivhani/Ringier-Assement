d78df59c152e44b9c0f475e66e41542579ee3a59
Netshivhani <netshivhanit@gmail.com>
2024-09-04 20:47:46 +0200

    Update build.sh

--- NODE.js APP/build.sh
+++ NODE.js APP/build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
+#!/bin/bash
 
 # Enable Docker BuildKit
 export DOCKER_BUILDKIT=1
 
-#PREBUILD
-#stuff like checking docker version
+# Build the Docker image and tag it: latest
+docker build --tag nodejs-app:latest --cache-from=type=local,src=.cache --cache-to=type=local,dest=.cache .
 
+# Display a message upon successful build
+echo "Docker image 'nodejs-app:latest' built successfully."
 
-# BUILD the Docker image
-export db-name, db-username, db-password
-docker build --progress=plain -t node-server .
-docker push to repo #provided Jfrog/aws repo URL
-
-#POSTBUILD
-#we can do stuff like triggering new containers like
-aws ecs update-services --cluster NETSHI_cluster --force new deployment
